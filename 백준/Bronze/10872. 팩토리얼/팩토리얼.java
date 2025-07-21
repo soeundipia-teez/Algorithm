f8c6a42c3310451fc14ee47fbf21baf1e82caf5f
김소은 <ildlisa@naver.com>
2025-07-21 22:48:35 +0900

    [Bronze III] Title: 팩토리얼, Time: 180 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        long fac = 1;
+
+        if (n <= 1) {
+            System.out.println("1");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                fac = fac * i;
+            }
+            System.out.println(fac);
+        }
+    }
+}
