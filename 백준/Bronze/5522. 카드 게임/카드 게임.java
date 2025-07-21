1525cdda5b2c1a6f04fa6874c80dbf431e5f2549
김소은 <ildlisa@naver.com>
2025-07-21 22:41:02 +0900

    [Bronze V] Title: 카드 게임, Time: 176 ms, Memory: 17704 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int e = scanner.nextInt();
+        
+        System.out.println(a + b + c + d + e);
+    }
+}
