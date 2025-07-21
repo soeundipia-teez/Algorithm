cc9bc5d81fab87a94d6c1eadf0296642eefc69c0
김소은 <ildlisa@naver.com>
2025-07-21 22:47:59 +0900

    [Bronze IV] Title: 특별한 날, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        
+        if (month == 2) {
+            if (day == 18) {
+                System.out.println("Special");
+            } else if (day < 18) {
+                System.out.println("Before");
+            } else {
+                System.out.println("After");
+            }
+        } else if (month < 2) {
+            System.out.println("Before");
+        } else {
+            System.out.println("After");
+        }
+    }
+}
