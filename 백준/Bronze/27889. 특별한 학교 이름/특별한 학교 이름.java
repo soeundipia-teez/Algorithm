4b01a0b846a853791ab916c75287e51f04327971
김소은 <ildlisa@naver.com>
2025-07-21 23:02:56 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 172 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String name = scanner.nextLine();
+        
+        if (name.equals("NLCS")) {
+            System.out.println("North London Collegiate School");
+        } else if (name.equals("BHA")) {
+            System.out.println("Branksome Hall Asia");
+        } else if (name.equals("KIS")) {
+            System.out.println("Korea International School");
+        } else {
+            System.out.println("St. Johnsbury Academy");
+        }
+    }
+}
