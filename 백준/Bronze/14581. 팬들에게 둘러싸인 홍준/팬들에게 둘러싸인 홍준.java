eb519fcf0ef2c7c3680bda5dfb554c76e62384c0
김소은 <ildlisa@naver.com>
2025-07-21 22:54:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 188 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String id = scanner.nextLine();
+        
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id +"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
