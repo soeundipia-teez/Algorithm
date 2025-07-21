fe58f89a545a1d4b0b0afb4442fcb833c239bc28
김소은 <ildlisa@naver.com>
2025-07-21 22:56:40 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 100 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("2025");
+        System.out.println("01");
+        System.out.println("09");
+    }
+}
