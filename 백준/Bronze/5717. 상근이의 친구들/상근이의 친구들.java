d7456e13a2a45fb7d67466103e48fb09ea4b197d
김소은 <ildlisa@naver.com>
2025-07-21 22:42:01 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 176 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int m = sc.nextInt();
+            int f = sc.nextInt();
+
+            if (m == 0 && f == 0) {
+                break;
+            } else {
+                System.out.println(m + f);
+            }
+        }
+    }
+}
