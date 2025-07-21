29e7faf20119b2722796f89144c49424f9dc72db
김소은 <ildlisa@naver.com>
2025-07-21 22:57:05 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 168 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        
+        System.out.println((d1 * 2) + (d2 * 2 * 3.141592));
+    }
+}
