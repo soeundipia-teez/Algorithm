71fd1f2f703accc00935c3071abf01f88b01d79b
김소은 <ildlisa@naver.com>
2025-07-21 23:00:41 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 176 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+        BigInteger c = scanner.nextBigInteger();
+        
+        System.out.println(b.subtract(c).divide(a));
+    }
+}
