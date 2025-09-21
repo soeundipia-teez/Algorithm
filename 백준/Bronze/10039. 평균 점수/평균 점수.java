dcef388036f0ca3635f30d62514761c67b3c5fde
김소은 <ildlisa@naver.com>
2025-09-21 16:03:51 +0900

    [Bronze IV] Title: 평균 점수, Time: 176 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] < 40) {
+                arr[i] = 40;
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println(sum / 5);
+    }
+}
