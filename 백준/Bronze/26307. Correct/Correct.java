6b3e21c55dbf94c09090593ce80168b3d74b2394
김소은 <ildlisa@naver.com>
2025-11-09 00:14:57 +0900

    [Bronze V] Title: Correct, Time: 172 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int HH = sc.nextInt();
+        int MM = sc.nextInt();
+
+        System.out.println(MM + (HH - 9) * 60);
+    }
+}
