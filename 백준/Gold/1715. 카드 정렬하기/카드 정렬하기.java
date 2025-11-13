c401242d4bbd9dd608cec21723a2fb59a831ea82
김소은 <ildlisa@naver.com>
2025-11-13 11:44:16 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 328 ms, Memory: 25756 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
+			pq.add(Long.parseLong(br.readLine()));
+		}
+		
+		long total = 0;
+		
+		while (pq.size() > 1) {
+			long first = pq.poll();
+			long second = pq.poll();
+			
+			long newnew = first + second;
+			
+			total += newnew;
+			
+			pq.add(newnew);
+		}
+		
+		System.out.println(total);
+	}
+}
